57959cd1fce4be46b8d1acbe70cba76dae12f474
Erick Christian <erickchristian48@gmail.com>
2025-03-08 22:39:49 +0700

    feat: Add basic http check example

--- examples/advanced/main.go
+++ examples/advanced/main.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+const url = "https://charm.sh/"
+
 type model struct {
-	choices  []string
-	cursor   int
-	selected map[int]struct{}
+	status int
+	err    error
+}
+
+func checkServer() tea.Msg {
+	c := &http.Client{Timeout: time.Second}
+	res, err := c.Get(url)
+
+	if err != nil {
+		return
+	}
+
 }
 
 func initialModel() model {
